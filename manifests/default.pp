9e04071efa19761d7dcc64f069c59d805e48a2bd
Pawel Graczyk <pawel.graczyk@groupon.com>
2013-03-01 20:19:10 +0100

    Change apache to puppetlabs version. Add puppetlabs nginx. Add dependencies for both.

--- manifests/default.pp
+++ manifests/default.pp
@@ -10,6 +10,20 @@ Package { ensure => "installed" }
 package { "varnish": }
 package { "git": }
 
-include apache2
+file { "/home/example.com":
+    ensure => "directory",
+}
+
+class { 'apache': }
+apache::vhost { 'local.example.com':
+    priority        => '10',
+    vhost_name      => '192.0.2.1',
+    port            => '80',
+    docroot         => '/home/example.com/docroot/',
+    logroot         => '/home/example.com/logroot/',
+    serveradmin     => 'webmaster@example.com',
+    serveraliases   => ['example.com',],
+}
+
 include mysql
 include php
